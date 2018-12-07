fd17e2e6604ef930352dad0f884e7f3ddc05f12b
G W <gbw327@student.hks.harvard.edu>
2018-12-07 17:13:00 -0500

    Upgraded app in development.

--- chinese-diplomacy-and-financing/appDev.R
+++ chinese-diplomacy-and-financing/appDev.R
@@ -34,6 +34,9 @@ aid_countries <- aiddf %>%
 aid_countries <- unique(aid_countries$country) %>% 
   sort()
 
+# Make a selection list of all the sectors for which China has provided aid
+aid_sectors <- unique(aiddf$sector)
+
 # Make a list of all countries between the three data sets
 countries <- as.list(read_rds("countries.rds"))
 
@@ -50,56 +53,45 @@ leaders <- unique(leaderdf$leader)
 ### USER INTERFACE
 ##################
 ui <- navbarPage("Elite Chinese Diplomacy and Financial Flows",
+                 position = "fixed-top",
 
 ### First tab
   tabPanel("Global Aid",
-    fluidPage(
-    # Page title
-    titlePanel("Global Aid (2000-2014)"),
+
+        div(class = "outer",
+        # Include custom CSS
+        tags$head(includeCSS("styles.css"))
+        ),
     
-    # Sidebar
-    sidebarLayout(
-      sidebarPanel(
-        width = 3,
-        selectInput(inputId = "country2", 
-                    label = "Select a country:",
-                    choices = aid_countries,
-                    selected = "Sri Lanka",
-                    selectize = TRUE),
-        awesomeCheckboxGroup(inputId = "leaders2",
-                             label = "Choose leaders:",
-                             choices = c(leaders),
-                             selected = c("Xi Jinping", "Hu Jintao")),
-        helpText("Xi Jinping: President (2013-Present); Vice President (2008-2013)"),
-        helpText("Hu Jintao: President (2003-2013); Vice President (1998-2003)"),
-        helpText("Li Keqiang: Premier (2013-Present); Vice Premier (2008-2013)"),
-        helpText("Wen Jiabao: Premier (2003-2013)")),
-      
-      # Main panel
-      mainPanel(leafletOutput("aidmap", width = "100%"),
-                timevisOutput("engagements2"),
-                tabPanel("",
-                         actionButton("year03.2", "2003"),
-                         actionButton("year04.2", "2004"),
-                         actionButton("year05.2", "2005"),
-                         actionButton("year06.2", "2006"),
-                         actionButton("year07.2", "2007"),
-                         actionButton("year08.2", "2008"),
-                         actionButton("year09.2", "2009"),
-                         actionButton("year10.2", "2010"),
-                         actionButton("year11.2", "2011"),
-                         actionButton("year12.2", "2012"),
-                         actionButton("year13.2", "2013"),
-                         actionButton("year14.2", "2014"),
-                         actionButton("year15.2", "2015"),
-                         actionButton("year16.2", "2016"),
-                         actionButton("year17.2", "2017"),
-                         actionButton("year18.2", "2018")),
-                hr(),
-                div(dataTableOutput("aidtable", width = "100%"), style = "font-size:80%"),
-                div(dataTableOutput("leadertable2", width = "100%"), style = "font-size:80%")
-      )
-    ))), # Three parentheses for tabPanel, fluidPage, and sidebarLayout
+    # Include the map
+    leafletOutput("aidmap", width = "100%", height = "600px"),
+    
+    # Include the selection panel
+    absolutePanel(id = "controls", class = "panel panel-default", fixed = FALSE, draggable = FALSE, top = 60, right = "auto",
+                  left = 40, bottom = "auto", height = "auto", width = 330,
+
+                  selectInput(inputId = "country2", 
+                              label = "Select a country:",
+                              choices = aid_countries,
+                              selected = "Sri Lanka",
+                              selectize = TRUE),
+                  selectInput(inputId = "sector",
+                              label = "Choose sectors:",
+                              choices = "All",
+                              selected = "All",
+                              multiple = FALSE),
+                  awesomeCheckboxGroup(inputId = "leaders2",
+                                       label = "Choose leaders:",
+                                       choices = c(leaders),
+                                       selected = c("Xi Jinping", "Hu Jintao"))
+    
+  ),
+  hr(),
+  timevisOutput("engagements2"),
+  hr(),
+  div(dataTableOutput("aidtable", width = "100%"), style = "font-size:80%"),
+  div(dataTableOutput("leadertable2", width = "100%"), style = "font-size:80%")
+  ),
 
 ### Second tab
 tabPanel("Global Investments", fluidPage(
@@ -156,56 +148,85 @@ tabPanel("Global Investments", fluidPage(
 ### SERVER
 ##########
 
-server <- function(input, output) {
+server <- function(input, output, session) {
 
 #########
 # AID TAB
 #########
 
+  filteredMapData <- reactive({
+    # Select data only for one project at a time
+    map_projects <- aiddf %>% 
+      filter(! is.na(latitude)) %>% 
+      # filter(str_detect(country, input$country2)) %>%
+      filter(! str_detect(country, ";"))
+    
+    # Remove duplicated projects so the markers don't overlap
+    map_projects[! duplicated(map_projects$id), ]
+  })
+  
   ### Draw the map
   output$aidmap <- renderLeaflet({
     
-    # Select data only for one project at a time
-    map_projects <- aiddf
-    # %>% 
-    #   filter(! is.na(longitude))
-    
-    # Remove duplicated projects so the markers don't overlap
-    map_projects <- map_projects[! duplicated(map_projects$id), ]
-      
-    # Set the latitude and longitude dynamically so users can focus in on different countries
-    data1 <- aiddf %>%
-      filter(str_detect(country, input$country2)) %>% 
-      filter(! str_detect(country, ";")) %>% 
-      filter(! is.na(longitude))
-    
     # Initiate the map
-    leaflet(map_projects) %>%
-      addProviderTiles(providers$Esri.WorldStreetMap) %>% 
-      setView(data1$longitude[1], data1$latitude[1], zoom = 7) %>% 
-      addCircleMarkers(lng = ~jitter(longitude, 2),
-                       lat = ~jitter(latitude, 2),
-                       radius = ~ifelse(is.na(usd_current), 8, log(usd_current) * 2),
+    leaflet(filteredMapData, options = leafletOptions(zoomControl = FALSE)) %>%
+      addProviderTiles(providers$Esri.WorldStreetMap)
+  })
+
+# Every time the user chooses a new country, redo the map view
+  observe({
+    map_projects <- filteredMapData() %>% 
+      filter(str_detect(country, input$country2))
+    
+    leafletProxy("aidmap", data = map_projects) %>%
+      setView(map_projects$longitude[1], map_projects$latitude[1], zoom = 7) 
+  })
+
+# Every time the user chooses a new sector, rewrite the markers
+  observe({
+    ifelse(input$sector == "All", 
+      {map_projects <- filteredMapData()},
+      {map_projects <- filteredMapData() %>% 
+        filter(sector == input$sector)})
+  
+    leafletProxy("aidmap", data = map_projects) %>%
+      clearMarkers() %>% 
+      clearMarkerClusters() %>% 
+      addCircleMarkers(lng = ~longitude,
+                       lat = ~latitude,
+                       radius = ~ifelse(is.na(usd_current), 6, log(usd_current)),
                        color = "#00A86B",
                        opacity = 0.5,
-                       fillOpacity = 0.5,
-                       clusterOptions = markerClusterOptions(freezeAtZoom = 10,
+                       fillOpacity = 0.25,
+                       clusterOptions = markerClusterOptions(freezeAtZoom = 12,
                                                              spiderfyOnMaxZoom = TRUE,
-                                                             removeOutsideVisibleBounds = FALSE,
+                                                             removeOutsideVisibleBounds = TRUE,
                                                              spiderfyDistanceMultiplier = 2,
-                                                             spiderLegPolylineOptions = list(weight = .5)),
+                                                             spiderLegPolylineOptions = list(weight = .75)),
                        popup = paste0("<b>Funder: </b>", map_projects$funder, "<br>",
                                       "<b>Sector: </b>", map_projects$sector, "<br>",
                                       "<b>Description: </b>", map_projects$title, "<br>",
                                       "<b>Amount: </b>", ifelse(is.na(map_projects$usd_current),
                                                                 "Unknown",
-                                                                paste0("$", formatC(map_projects$usd_current, format = "f", digits = 0, big.mark = ","))), "<br>",
+                                                                paste0("$", formatC(map_projects$usd_current,
+                                                                                    format = "f",
+                                                                                    digits = 0,
+                                                                                    big.mark = ","))), "<br>",
                                       "<b>Intent: </b>", map_projects$intent, "<br>",
                                       "<b>Locale: </b>", str_trim(map_projects$place), ", ",
                                       map_projects$country, ", ",
                                       map_projects$year))
+    
   })
-  
+
+  ### Make a custom list of sectors for each country
+  observe({
+    data1 <- filteredMapData() %>% 
+      filter(str_detect(country, input$country2))
+    
+    updateSelectInput(session, "sector", choices = c("All", unique(data1$sector)), selected = "All")
+  })
+
   
   ### Make the timeline
   output$engagements2 <- renderTimevis({
@@ -283,7 +304,7 @@ server <- function(input, output) {
   observeEvent(input$year18.2, {
     centerTime("engagements2", "2018-06-15")})
 
-### Make the leader engagements data table
+### Make the aid data table
   output$aidtable = renderDataTable({
     
     # Select data only for the country chosen
